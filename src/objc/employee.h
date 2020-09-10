574e393b16a1740a358e71b0777e126dc36cbd57
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-09-10 17:44:44 +0200

    Fix ARC issues in the Objective-C example
    Fixing warnings and tweaking style while at it.

--- src/objc/employee.h
+++ src/objc/employee.h
@@ -1,20 +1,19 @@
 #import <Foundation/Foundation.h>
-@interface Employee : NSObject
-	//property declaration
-	@property (retain) NSString* username;
-	@property (retain) NSString *firstName;
-	@property (readonly) short shortWord;
-	@property (readonly) uint64_t wideWord;
-	// @private NSString *lastName;
-	// @private (retain, nonatomic) NSString* name;
 
-	// methods
-	+ sayHello;
-//	+ (void)loadTableData:(int);
-	//method declaration
-	- (void) helloWorld;
-	- (void*)p0;
-	- (void*)p1;
-	- (void*)p2;
-	- (void*)p3;
+@interface Employee : NSObject
+
+@property (retain) NSString* username;
+@property (retain) NSString* firstName;
+@property (readonly) short shortWord;
+@property (readonly) uint64_t wideWord;
+
++ (void)sayHello;
+- (void)sayHello;
+- (void)helloWorld;
+- (void*)p0;
+- (void*)p1;
+- (void*)p2;
+- (void*)p3;
+- (void*)base;
+
 @end
