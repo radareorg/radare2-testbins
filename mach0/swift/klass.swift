9869fc88ef2d3b59dffd82a154bceba50b4704f8
Sergi Àlvarez i Capilla <pancake@nowsecure.com>
2022-03-26 21:39:05 +0100

    Add swift klass sample bin

---
+++ mach0/swift/klass.swift
@@ -0,0 +1,15 @@
+// objc class
+class SomeClass : SuperKlass {
+  // swift create methods for getters/setters
+  var meh: Int = 4
+  var cow: Int = 3
+  func instanceMethod() {
+  }
+}
+
+
+class SuperKlass {
+	var superfield : Int = 4
+}
+
+var sk = SuperKlass()
