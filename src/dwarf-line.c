f3fc960069a36d2b2374e874005e870f03947b02
wagner riffel <w@104d.net>
2024-11-02 00:14:38 -0300

    Update DWARF line program tests
    Signed-off-by: wagner riffel <w@104d.net>

--- src/dwarf-line.c
+++ src/dwarf-line.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
 
+#include "/tmp/dwarf-line-foo.c"
+#include "dwarf-line//bar.c"
+
+struct Foo {};
+
+struct Foo *new_foo() {
+	return NULL;
+}
+void foo();
+
+struct Bar {};
+
+struct Bar *new_bar() {
+	return NULL;
+}
+void bar();
+
 int main(int a, const char **v) {
 	while (a-- > 0) {
 		printf ("Hello %d\n", a);
 	}
+	foo();
+	bar();
 	return 0;
 }
---
+++ src/dwarf-line/bar.c
@@ -0,0 +1,10 @@
+struct Bar;
+
+struct Bar *new_bar(void);
+
+void bar() {
+	int printf(const char *, ...);
+
+	struct Bar *f = new_bar();
+	printf("%p\n", f);
+}
