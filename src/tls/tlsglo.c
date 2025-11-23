ac4cd1369cf419931d71406e25a6505b7d9c4547
pancake <pancake@nopcode.org>
2025-11-23 09:36:37 +0100

    Improved tls global test program

--- src/tls/tlsglo.c
+++ src/tls/tlsglo.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 _Thread_local int tls = 0;
 int glo = 0;
+
+static void set_globals(void) {
+	tls = 1;
+	glo = 2;
+}
+
+static void print_globals(void) {
+	printf ("tls val %d\n", tls);
+	printf ("glo val %d\n", glo);
+}
+
 int main() {
-	printf ("%p\n", &tls);
-	printf ("%p\n", &glo);
+	set_globals ();
+	printf ("tls ptr %p\n", &tls);
+	printf ("glo ptr %p\n", &glo);
+	print_globals ();
 	return 0;
 }
